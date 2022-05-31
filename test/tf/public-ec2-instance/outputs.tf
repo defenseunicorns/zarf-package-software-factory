d9dcdd01d181eb9db3dda071f71e980585e69ffa
Andy Roth <roth.andy@gmail.com>
2022-05-31 13:33:36 -0700

    wip

--- test/tf/public-ec2-instance/outputs.tf
+++ test/tf/public-ec2-instance/outputs.tf
@@ -1,7 +1,7 @@
 output "public_instance_id" {
-  value = aws_spot_instance_request.public.id
+  value = aws_instance.public.id
 }
 
 output "public_instance_ip" {
-  value = aws_spot_instance_request.public.public_ip
+  value = aws_instance.public.public_ip
 }
