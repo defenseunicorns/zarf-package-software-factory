b96a2f156c19808a0986ffa2c5be842805a45cb0
Andy Roth <roth.andy@gmail.com>
2022-12-07 15:33:41 -0800

    wip

--- test/e2e/e2e_basic_smoke_test.go
+++ test/e2e/e2e_basic_smoke_test.go
@@ -43,24 +43,24 @@ func TestAllServicesRunning(t *testing.T) { //nolint:funlen
 		require.NoError(t, err, output)
 		timestampArtifactoryDb := time.Now().Add(time.Minute * 5).Add(time.Second * 10)
 		// Wait for the "acid-confluence" database to exist.
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c \"while ! kubectl get postgresql acid-confluence -n confluence; do sleep 5; done\"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! kubectl get postgresql acid-confluence -n confluence; do sleep 5; done"`)
 		require.NoError(t, err, output)
 		// Wait for the "acid-confluence" database to report "PostgresClusterStatus==Running", then set the timestamp
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c \"DB_STATUS=$(kubectl get postgresql acid-confluence -n confluence -o jsonpath=:"{.status.PostgresClusterStatus}"); while [ $DB_STATUS != "Running" ]; do sleep 5; done\"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "DB_STATUS=$(kubectl get postgresql acid-confluence -n confluence -o jsonpath=:"{.status.PostgresClusterStatus}"); while [ $DB_STATUS != "Running" ]; do sleep 5; done"`)
 		require.NoError(t, err, output)
 		timestampConfluenceDb := time.Now().Add(time.Minute * 5).Add(time.Second * 10)
 		// Wait for the "acid-gitlab" database to exist.
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c \"while ! kubectl get postgresql acid-gitlab -n gitlab; do sleep 5; done\"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! kubectl get postgresql acid-gitlab -n gitlab; do sleep 5; done"`)
 		require.NoError(t, err, output)
 		// Wait for the "acid-gitlab" database to report "PostgresClusterStatus==Running", then set the timestamp
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c \"DB_STATUS=$(kubectl get postgresql acid-gitlab -n gitlab -o jsonpath="{.status.PostgresClusterStatus}"); while [ $DB_STATUS != "Running" ]; do sleep 5; done\"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "DB_STATUS=$(kubectl get postgresql acid-gitlab -n gitlab -o jsonpath="{.status.PostgresClusterStatus}"); while [ $DB_STATUS != "Running" ]; do sleep 5; done"`)
 		require.NoError(t, err, output)
 		timestampGitlabDb := time.Now().Add(time.Minute * 5).Add(time.Second * 10)
 		// Wait for the "acid-jira" database to exist.
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c \"while ! kubectl get postgresql acid-jira -n jira; do sleep 5; done\"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! kubectl get postgresql acid-jira -n jira; do sleep 5; done"`)
 		require.NoError(t, err, output)
 		// Wait for the "acid-jira" database to report "PostgresClusterStatus==Running", then set the timestamp
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c \"DB_STATUS=$(kubectl get postgresql acid-jira -n jira -o jsonpath="{.status.PostgresClusterStatus}"); while [ $DB_STATUS != "Running" ]; do sleep 5; done\"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "DB_STATUS=$(kubectl get postgresql acid-jira -n jira -o jsonpath="{.status.PostgresClusterStatus}"); while [ $DB_STATUS != "Running" ]; do sleep 5; done"`)
 		require.NoError(t, err, output)
 		timestampJiraDb := time.Now().Add(time.Minute * 5).Add(time.Second * 10)
 
@@ -71,98 +71,98 @@ func TestAllServicesRunning(t *testing.T) { //nolint:funlen
 		output, err = platform.RunSSHCommandAsSudo(`kubectl wait kustomization/softwarefactoryaddons -n flux-system --for=condition=Ready --timeout=1200s`)
 		require.NoError(t, err, output)
 		// Wait for the bbcore-minio Statefulset "bbcore-minio-minio-instance-ss-0" to exist.
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c \"while ! kubectl get statefulset bbcore-minio-minio-instance-ss-0 -n bbcore-minio; do sleep 5; done\"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! kubectl get statefulset bbcore-minio-minio-instance-ss-0 -n bbcore-minio; do sleep 5; done"`)
 		require.NoError(t, err, output)
 		// Wait for the bbcore-minio Statefulset "bbcore-minio-minio-instance-ss-0" to report that it is ready.
 		output, err = platform.RunSSHCommandAsSudo(`kubectl rollout status statefulset/bbcore-minio-minio-instance-ss-0 -n bbcore-minio --watch --timeout=1200s`)
 		require.NoError(t, err, output)
 		// Wait for the gitlab-minio Statefulset "gitlab-minio-minio-instance-ss-0" to exist.
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c \"while ! kubectl get statefulset gitlab-minio-minio-instance-ss-0 -n gitlab-minio; do sleep 5; done\"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! kubectl get statefulset gitlab-minio-minio-instance-ss-0 -n gitlab-minio; do sleep 5; done"`)
 		require.NoError(t, err, output)
 		// Wait for the gitlab-minio Statefulset "gitlab-minio-minio-instance-ss-0" to report that it is ready.
 		output, err = platform.RunSSHCommandAsSudo(`kubectl rollout status statefulset/gitlab-minio-minio-instance-ss-0 -n gitlab-minio --watch --timeout=1200s`)
 		require.NoError(t, err, output)
 		// Wait for the velero-minio Statefulset "velero-minio-minio-instance-ss-0" to exist.
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c \"while ! kubectl get statefulset velero-minio-minio-instance-ss-0 -n velero-minio; do sleep 5; done\"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! kubectl get statefulset velero-minio-minio-instance-ss-0 -n velero-minio; do sleep 5; done"`)
 		require.NoError(t, err, output)
 		// Wait for the velero-minio Statefulset "velero-minio-minio-instance-ss-0" to report that it is ready.
 		output, err = platform.RunSSHCommandAsSudo(`kubectl rollout status statefulset/velero-minio-minio-instance-ss-0 -n velero-minio --watch --timeout=1200s`)
 		require.NoError(t, err, output)
 		// Wait for the GitLab Webservice Deployment to exist.
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c \"while ! kubectl get deployment gitlab-webservice-default -n gitlab; do sleep 5; done\"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! kubectl get deployment gitlab-webservice-default -n gitlab; do sleep 5; done"`)
 		require.NoError(t, err, output)
 		// Wait for the GitLab Webservice Deployment to report that it is ready
 		output, err = platform.RunSSHCommandAsSudo(`kubectl rollout status deployment/gitlab-webservice-default -n gitlab --watch --timeout=1200s`)
 		require.NoError(t, err, output)
 		// Wait for the Velero Deployment to exist.
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c \"while ! kubectl get deployment velero-velero -n velero; do sleep 5; done\"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! kubectl get deployment velero-velero -n velero; do sleep 5; done"`)
 		require.NoError(t, err, output)
 		// Wait for the Velero Deployment to report that it is ready
 		output, err = platform.RunSSHCommandAsSudo(`kubectl rollout status deployment/velero-velero -n velero --watch --timeout=1200s`)
 		require.NoError(t, err, output)
 		// Wait for the Restic Daemonset to exist.
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c \"while ! kubectl get daemonset restic -n velero; do sleep 5; done\"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! kubectl get daemonset restic -n velero; do sleep 5; done"`)
 		require.NoError(t, err, output)
 		// Wait for the Restic Daemonset to report that it is ready
 		output, err = platform.RunSSHCommandAsSudo(`kubectl rollout status daemonset/restic -n velero --watch --timeout=1200s`)
 		require.NoError(t, err, output)
 		// Wait for the Jenkins StatefulSet to exist.
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c \"while ! kubectl get statefulset jenkins -n jenkins; do sleep 5; done\"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! kubectl get statefulset jenkins -n jenkins; do sleep 5; done"`)
 		require.NoError(t, err, output)
 		// Wait for the Jenkins StatefulSet to report that it is ready
 		output, err = platform.RunSSHCommandAsSudo(`kubectl rollout status statefulset/jenkins -n jenkins --watch --timeout=1200s`)
 		require.NoError(t, err, output)
 		// Ensure that Jenkins is able to talk to GitLab internally
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c \"while ! kubectl exec statefulset/jenkins -n jenkins -c jenkins -- curl -L -s --fail --show-error https://gitlab.bigbang.dev/-/health > /dev/null; do sleep 5; done\"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! kubectl exec statefulset/jenkins -n jenkins -c jenkins -- curl -L -s --fail --show-error https://gitlab.bigbang.dev/-/health > /dev/null; do sleep 5; done"`)
 		require.NoError(t, err, output)
 		// Wait for the Jira StatefulSet to exist.
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c \"while ! kubectl get statefulset jira -n jira; do sleep 5; done\"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! kubectl get statefulset jira -n jira; do sleep 5; done"`)
 		require.NoError(t, err, output)
 		// Wait for the Jira StatefulSet to report that it is ready
 		output, err = platform.RunSSHCommandAsSudo(`kubectl rollout status statefulset/jira -n jira --watch --timeout=1200s`)
 		require.NoError(t, err, output)
 		// Ensure that Jira is able to talk to GitLab internally
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c \"while ! kubectl exec statefulset/jira -n jira -c jira -- curl -L -s --fail --show-error https://gitlab.bigbang.dev/-/health > /dev/null; do sleep 5; done\"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! kubectl exec statefulset/jira -n jira -c jira -- curl -L -s --fail --show-error https://gitlab.bigbang.dev/-/health > /dev/null; do sleep 5; done"`)
 		require.NoError(t, err, output)
 		// Wait for the Confluence StatefulSet to exist.
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c \"while ! kubectl get statefulset confluence -n confluence; do sleep 5; done\"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! kubectl get statefulset confluence -n confluence; do sleep 5; done"`)
 		require.NoError(t, err, output)
 		// Wait for the Confluence StatefulSet to report that it is ready
 		output, err = platform.RunSSHCommandAsSudo(`kubectl rollout status statefulset/confluence -n confluence --watch --timeout=1200s`)
 		require.NoError(t, err, output)
 		// Ensure that Confluence is able to talk to GitLab internally
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c \"while ! kubectl exec statefulset/confluence -n confluence -c confluence -- curl -L -s --fail --show-error https://gitlab.bigbang.dev/-/health > /dev/null; do sleep 5; done\"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! kubectl exec statefulset/confluence -n confluence -c confluence -- curl -L -s --fail --show-error https://gitlab.bigbang.dev/-/health > /dev/null; do sleep 5; done"`)
 		require.NoError(t, err, output)
 		// Make sure flux is present.
 		output, err = platform.RunSSHCommandAsSudo("flux --help")
 		require.NoError(t, err, output)
 		// Ensure that Jenkins is available outside of the cluster.
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c \"while ! curl -L -s --fail --show-error https://jenkins.bigbang.dev/login > /dev/null; do sleep 5; done\"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! curl -L -s --fail --show-error https://jenkins.bigbang.dev/login > /dev/null; do sleep 5; done"`)
 		require.NoError(t, err, output)
 		// Ensure that Confluence is available outside of the cluster.
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c \"while ! curl -L -s --fail --show-error https://confluence.bigbang.dev/status > /dev/null; do sleep 5; done\"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! curl -L -s --fail --show-error https://confluence.bigbang.dev/status > /dev/null; do sleep 5; done"`)
 		require.NoError(t, err, output)
 		// Ensure that Jira is available outside of the cluster.
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c \"while ! curl -L -s --fail --show-error https://jira.bigbang.dev/status > /dev/null; do sleep 5; done\"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! curl -L -s --fail --show-error https://jira.bigbang.dev/status > /dev/null; do sleep 5; done"`)
 		require.NoError(t, err, output)
 		// Ensure that GitLab is available outside of the cluster.
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c \"while ! curl -L -s --fail --show-error https://gitlab.bigbang.dev/-/health > /dev/null; do sleep 5; done\"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! curl -L -s --fail --show-error https://gitlab.bigbang.dev/-/health > /dev/null; do sleep 5; done"`)
 		require.NoError(t, err, output)
 		// Wait for the Artifactory StatefulSet to exist.
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c \"while ! kubectl get statefulset artifactory -n artifactory; do sleep 5; done\"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! kubectl get statefulset artifactory -n artifactory; do sleep 5; done"`)
 		require.NoError(t, err, output)
 		// Wait for the Artifactory StatefulSet to report that it is ready
 		output, err = platform.RunSSHCommandAsSudo(`kubectl rollout status statefulset/artifactory -n artifactory --watch --timeout=1200s`)
 		require.NoError(t, err, output)
 		// Ensure that Artifactory is able to talk to GitLab internally
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c \"while ! kubectl exec statefulset/artifactory -n artifactory -c artifactory -- curl -L -s --fail --show-error https://gitlab.bigbang.dev/-/health > /dev/null; do sleep 5; done\"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! kubectl exec statefulset/artifactory -n artifactory -c artifactory -- curl -L -s --fail --show-error https://gitlab.bigbang.dev/-/health > /dev/null; do sleep 5; done"`)
 		require.NoError(t, err, output)
 		// Ensure that Artifactory is available outside of the cluster.
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c \"while ! curl -L -s --fail --show-error https://artifactory.bigbang.dev/artifactory/api/system/ping > /dev/null; do sleep 5; done\"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! curl -L -s --fail --show-error https://artifactory.bigbang.dev/artifactory/api/system/ping > /dev/null; do sleep 5; done"`)
 		require.NoError(t, err, output)
 
 		// Wait for the Loki write Statefulset to exist.
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c \"while ! kubectl get statefulset logging-loki-write -n logging; do sleep 5; done\"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! kubectl get statefulset logging-loki-write -n logging; do sleep 5; done"`)
 		require.NoError(t, err, output)
 
 		// Wait for the Loki wrie Deployment to report that it is ready
@@ -170,7 +170,7 @@ func TestAllServicesRunning(t *testing.T) { //nolint:funlen
 		require.NoError(t, err, output)
 
 		// Wait for the Loki read Statefulset to exist.
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c \"while ! kubectl get statefulset logging-loki-read -n logging; do sleep 5; done\"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! kubectl get statefulset logging-loki-read -n logging; do sleep 5; done"`)
 		require.NoError(t, err, output)
 
 		// Wait for the Loki read Deployment to report that it is ready
@@ -178,7 +178,7 @@ func TestAllServicesRunning(t *testing.T) { //nolint:funlen
 		require.NoError(t, err, output)
 
 		// Wait for the Promtail Daemonset to exist.
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c \"while ! kubectl get daemonset logging-promtail -n logging; do sleep 5; done\"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! kubectl get daemonset logging-promtail -n logging; do sleep 5; done"`)
 		require.NoError(t, err, output)
 
 		// Wait for the Promtail Statefulset to report that it is ready
