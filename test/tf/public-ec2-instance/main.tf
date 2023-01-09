5129013b97c18dc9206925590922819c39fe6a71
Andy Roth <roth.andy@gmail.com>
2023-01-09 08:52:36 -0800

    hard code ubuntu AMI ID

--- test/tf/public-ec2-instance/main.tf
+++ test/tf/public-ec2-instance/main.tf
@@ -21,7 +21,8 @@ data "aws_vpc" "default" {
 # ---------------------------------------------------------------------------------------------------------------------
 
 resource "aws_instance" "public" {
-  ami                    = data.aws_ami.ubuntu.id
+  # ami                    = data.aws_ami.ubuntu.id
+  ami                    = "ami-061efa908c09c5409"
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.public.id]
   key_name               = var.key_pair_name
@@ -92,30 +93,30 @@ resource "aws_security_group" "public" {
 # LOOK UP THE LATEST UBUNTU AMI
 # ---------------------------------------------------------------------------------------------------------------------
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"] # Canonical
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
+#   owners      = ["099720109477"] # Canonical
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
 
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
+#   filter {
+#     name   = "architecture"
+#     values = ["x86_64"]
+#   }
 
-  filter {
-    name   = "image-type"
-    values = ["machine"]
-  }
+#   filter {
+#     name   = "image-type"
+#     values = ["machine"]
+#   }
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-}
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+#   }
+# }
 
 # ---------------------------------------------------------------------------------------------------------------------
 # LOOK UP THE CALLER ID
