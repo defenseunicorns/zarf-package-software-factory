98a63267674a1df51eb483113a5b9d86149a620d
corang <jordan@defenseunicorns.com>
2023-03-02 17:32:49 -0600

    working uid based backup

--- backup-and-restore/jira/files/restore.sh
+++ backup-and-restore/jira/files/restore.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+echo "Currently Jira is scaled to zero replicas and the jira-database helmrelease/clutser has been deleted"
+echo "This script will walk you through restoring data from this package to the jira database and bringing Jira back up"
+
+yq '.spec.values.resources[0].spec.clone = {"cluster": "test", "uid": "testuid"}' kustomizations/softwarefactoryaddons/base/databases/jira.yaml
