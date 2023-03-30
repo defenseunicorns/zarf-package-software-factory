f3b2faa5a5f8a34009de6900e8bf0bbcff55d04a
Anthony <anthony@defenseunicorns.com>
2023-03-30 15:50:07 -0600

    Update terratest to default to availability zone and allow env var setting of it

--- test/tf/public-ec2-instance/variables.tf
+++ test/tf/public-ec2-instance/variables.tf
@@ -17,6 +17,11 @@ variable "aws_region" {
   type        = string
 }
 
+variable "aws_availability_zone" {
+  description = "The AWS availability zone to use"
+  type        = string
+}
+
 variable "namespace" {
   type        = string
   description = "Namespace, which could be your organization name or abbreviation, e.g. 'eg' or 'cp'"
