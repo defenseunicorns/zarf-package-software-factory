037fa691459bc1bab81982b3db6cdf64f1c9f95d
corang <jordan@defenseunicorns.com>
2023-04-12 01:09:16 -0500

    better install script

--- test/metallb/install.sh
+++ test/metallb/install.sh
@@ -2,19 +2,15 @@
 
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.9/config/manifests/metallb-native.yaml
 
-sleep 5s
+kubectl rollout status deployment/controller -n metallb-system --watch --timeout=90s
 
-kubectl wait --namespace metallb-system \
-                --for=condition=ready pod \
-                --selector=app=metallb \
-                --timeout=90s
+kubectl rollout status daemonset/speaker -n metallb-system --watch --timeout=90s
 
 NETWORK_BASE=$(docker network inspect -f '{{(index .IPAM.Config 0).Subnet}}' kind | cut -d '.' --fields '1 2 3')
 
 NETWORK_RANGE_START="${NETWORK_BASE}.200"
 NETWORK_RANGE_END="${NETWORK_BASE}.210"
 
-sed -i "s/#RANGE_START#/${NETWORK_RANGE_START}/g" test/metallb/config.yaml
-sed -i "s/#RANGE_END#/${NETWORK_RANGE_END}/g" test/metallb/config.yaml
+METALLB_CONFIG=$(sed "s/#RANGE_START#/${NETWORK_RANGE_START}/g" test/metallb/config.yaml | sed "s/#RANGE_END#/${NETWORK_RANGE_END}/g")
 
-kubectl apply -f test/metallb/config.yaml
+echo "$METALLB_CONFIG" | kubectl apply -f -
