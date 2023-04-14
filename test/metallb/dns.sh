a2756cbf04aec4c8045759e9493160411ef71d2c
corang <jordan@defenseunicorns.com>
2023-04-13 21:07:29 -0500

    fix dns script for multi ingressgateway

--- test/metallb/dns.sh
+++ test/metallb/dns.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-HOST_LIST=$(kubectl get vs -A -o=jsonpath='{range .items[*]}{.spec.hosts[*]}{"\n"}{end}' | sort -u)
+HOST_LIST=$(kubectl get vs -A -o=jsonpath='{range .items[*]}{.spec.gateways[*]}{" "}{.spec.hosts[*]}{"\n"}{end}' | sort -u)
 
-LB_IP=$(kubectl get svc -n istio-system public-ingressgateway -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
+PUBLIC_HOSTS=$(echo "${HOST_LIST}" | grep public | cut -d ' ' -f2)
+PRIVATE_HOSTS=$(echo "${HOST_LIST}" | grep private | cut -d ' ' -f2)
 
-echo >> /etc/hosts
+PUBLIC_LB_IP=$(kubectl get svc -n istio-system public-ingressgateway -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
+PRIVATE_LB_IP=$(kubectl get svc -n istio-system private-ingressgateway -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
+
+sed -i -z 's/\n# Following entries are from metallb dns.sh.*# End of metallb dns.sh//' /etc/hosts
+
 echo "# Following entries are from metallb dns.sh" >> /etc/hosts
 
-for host in $HOST_LIST; do
+echo "# Public hostnames" >> /etc/hosts
+
+for host in $PUBLIC_HOSTS; do
+    echo "${LB_IP} ${host}" >> /etc/hosts
+done
+
+echo "# Private hostnames" >> /etc/hosts
+
+for host in $PRIVATE_HOSTS; do
     echo "${LB_IP} ${host}" >> /etc/hosts
 done
 
