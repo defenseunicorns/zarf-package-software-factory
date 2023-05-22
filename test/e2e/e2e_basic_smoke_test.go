dc8b6ca491343f2c70044bf30ce0dbb7d3ebcc95
Michael Kruggel <michael.kruggel@defenseunicorns.com>
2023-05-22 09:35:35 -0700

    Upgrading all packages to be inline with BB-2.2.0

--- test/e2e/e2e_basic_smoke_test.go
+++ test/e2e/e2e_basic_smoke_test.go
@@ -212,11 +212,11 @@ func TestAllServicesRunning(t *testing.T) { //nolint:funlen
 		require.NoError(t, err, output)
 
 		// Wait for the Promtail Daemonset to exist.
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! kubectl get daemonset logging-promtail -n logging; do sleep 5; done"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! kubectl get daemonset promtail -n promtail; do sleep 5; done"`)
 		require.NoError(t, err, output)
 
 		// Wait for the Promtail Statefulset to report that it is ready
-		output, err = platform.RunSSHCommandAsSudo(`kubectl rollout status daemonset/logging-promtail -n logging --watch --timeout=1200s`)
+		output, err = platform.RunSSHCommandAsSudo(`kubectl rollout status daemonset/promtail -n promtail --watch --timeout=1200s`)
 		require.NoError(t, err, output)
 
 		// Ensure that the services do not accept discontinued TLS versions. If they reject TLSv1.1 it is assumed that they also reject anything below TLSv1.1.
