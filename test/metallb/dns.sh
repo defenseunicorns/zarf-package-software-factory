d5a41471a099e8dd0c88d1b06007630549906946
Jordan McClintock <jordan@defenseunicorns.com>
2023-05-31 21:32:02 +0000

    add passthrough gateway to dns script

--- test/metallb/dns.sh
+++ test/metallb/dns.sh
@@ -4,9 +4,11 @@ HOST_LIST=$(kubectl get vs -A -o=jsonpath='{range .items[*]}{.spec.gateways[*]}{
 
 PUBLIC_HOSTS=$(echo "${HOST_LIST}" | grep public | cut -d ' ' -f2)
 PRIVATE_HOSTS=$(echo "${HOST_LIST}" | grep private | cut -d ' ' -f2)
+PASSTHROUGH_HOSTS=$(echo "${HOST_LIST}" | grep passthrough | cut -d ' ' -f2)
 
 PUBLIC_LB_IP=$(kubectl get svc -n istio-system public-ingressgateway -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
 PRIVATE_LB_IP=$(kubectl get svc -n istio-system private-ingressgateway -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
+PASSTHROUGH_LB_IP=$(kubectl get svc -n istio-system passthrough-ingressgateway -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
 sed -i -z 's/\n# Following entries are from metallb dns.sh.*# End of metallb dns.sh//' /etc/hosts
 
@@ -22,6 +24,12 @@ echo "# Private hostnames" >> /etc/hosts
 
 for host in $PRIVATE_HOSTS; do
     echo "${PRIVATE_LB_IP} ${host}" >> /etc/hosts
+done
+
+echo "# Passthrough hostnames" >> /etc/hosts
+
+for host in $PASSTHROUGH_HOSTS; do
+    echo "${PASSTHROUGH_LB_IP} ${host}" >> /etc/hosts
 done
 
 echo "# End of metallb dns.sh" >> /etc/hosts
